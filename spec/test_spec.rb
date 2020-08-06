04689010deca931d56f18aff726698528ab4161c
hirano wataru <wataru.hirano@globis.co.jp>
2020-08-06 09:00:08 +0900

    no message

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,22 +2,22 @@
 
 require_relative 'spec_helper.rb'
 
-RSpec.describe 'グロービス学び放題' do
-  it '動画が再生できること。' do
+RSpec.describe 'globis_manabihoudai' do
+  it 'can play movie' do
     puts('グロービス学び放題を開く')
     puts('グロービス学び放題で、ログインする')
     puts('動画画面に遷移する。')
     puts('動画を視聴する。')
     puts('動画が再生されること。')
   end
-  it 'いいねできること' do
+  it 'can touch good button' do
     puts('グロービス学び放題を開く')
     puts('グロービス学び放題で、ログインする')
     puts('動画画面に遷移する。')
     puts('いいねボタンを押せること。')
     puts('いいねボタンが押せていること。')
   end
-  it 'いいねできること' do
+  it 'can watch kato-san movie' do
     puts('グロービス学び放題を開く')
     puts('グロービス学び放題で、ログインする')
     puts('達人の一冊を開く')
