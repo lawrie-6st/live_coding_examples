a686eda0aa70c4f4a65f72e0e61d2858c33d817b
hirano wataru <wataru.hirano@globis.co.jp>
2020-08-06 11:05:04 +0900

    add some comment

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
 require_relative 'spec_helper.rb'
-
+# bundle exec rspec
+# 通常のRSPECです。
 RSpec.describe 'globis_manabihoudai' do
-  it 'can play movie' do
-    puts('グロービス学び放題を開く')
+  before do
+    puts('グロービス学び放題を開く') # TODO: 他のファイルでこのメソッドを共通して利用することができない。
     puts('グロービス学び放題で、ログインする')
+  end
+
+  it 'can play movie' do
     puts('動画画面に遷移する。')
     puts('動画を視聴する。')
     puts('動画が再生されること。')
   end
+
   it 'can touch good button' do
-    puts('グロービス学び放題を開く')
-    puts('グロービス学び放題で、ログインする')
     puts('動画画面に遷移する。')
     puts('いいねボタンを押せること。')
     puts('いいねボタンが押せていること。')
   end
+
   it 'can watch kato-san movie' do
-    puts('グロービス学び放題を開く')
-    puts('グロービス学び放題で、ログインする')
     puts('達人の一冊を開く')
     puts('加藤さんの動画を再生する。')
     puts('アンドロイドは電気羊の夢をみるかの文字を発見する・')
